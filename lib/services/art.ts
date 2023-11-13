a0a7fdfedf27e855224b242b1a4cbb4e153de1be
Ilya Pashayan <ilya.w.a.j@gmail.com>
2023-11-14 00:42:26 +0400

    fix

--- lib/services/art.ts
+++ lib/services/art.ts
@@ -40,76 +40,61 @@ export const generateAndSaveArt = async ({
   size: any;
 }) => {
   try {
-    // const monthlyLimit = 2 ;
-
-    // const currentDate = new Date();
-    // const currentMonth = currentDate.getMonth() + 1;
-    // const currentYear = currentDate.getFullYear();
-
-    // const { count: count, error: monthlyArtsError } = await supabase
-    //   .from('arts')
-    //   .select('id', { count: 'exact' })
-    //   .eq('user_id', user.id)
-    //   .gte('created_at', `${currentYear}-${currentMonth}-01T00:00:00Z`);
-
-    // if (monthlyArtsError) {
-    //   throw monthlyArtsError;
-    // }
-   
-    // const monthlyArtsCount = count || 0 ;
-
-
-    // if (monthlyArtsCount >= monthlyLimit) {
-    //     throw {limit_exceeded: true}
-    // }
-
-    const response = await openai.images.generate({
+    const generateImagePromise = openai.images.generate({
       model: 'dall-e-3',
       prompt,
       n,
       size
     });
 
-    if (response) {
-      const imageUrl = response.data[0].url
+    // You can perform other asynchronous tasks while waiting for the OpenAI API call
+    // For example, fetching additional data or performing other computations
 
-      if (!imageUrl) {
-        throw new Error('Image URL not found in the response');
-      }
+    const response = await generateImagePromise;
 
-      // const imageResponse = await fetch(imageUrl);
-      // const imageBuffer = await imageResponse.buffer();
-
-      // const { data: storageData, error: storageError } = await supabase.storage
-      //   .from('Arts')
-      //   .upload(`images/${user.id}/${Date.now()}.jpg`, imageBuffer);
-
-      // if (storageError) {
-      //   throw storageError;
-      // }
-
-      // const path = storageData.path;
-
-      // const { data: upsertData, error: upsertError } = await supabase
-      //   .from('arts')
-      //   .upsert([
-      //     {
-      //       user_id: user.id,
-      //       path: imageUrl 
-      //     }
-      //   ]);
-
-      // if (upsertError) {
-      //   throw upsertError;
-      // }
-
-      return imageUrl;
-    } else {
+    if (!response) {
       throw new Error(`OpenAI API error: ${response}`);
     }
+
+    const imageUrl = response.data[0]?.url;
+
+    if (!imageUrl) {
+      throw new Error('Image URL not found in the response');
+    }
+
+    // Other asynchronous tasks can be performed here, if needed
+
+    // const imageResponse = await fetch(imageUrl);
+    // const imageBuffer = await imageResponse.buffer();
+
+    // const { data: storageData, error: storageError } = await supabase.storage
+    //   .from('Arts')
+    //   .upload(`images/${user.id}/${Date.now()}.jpg`, imageBuffer);
+
+    // if (storageError) {
+    //   throw storageError;
+    // }
+
+    // const path = storageData.path;
+
+    // const { data: upsertData, error: upsertError } = await supabase
+    //   .from('arts')
+    //   .upsert([
+    //     {
+    //       user_id: user.id,
+    //       path: imageUrl 
+    //     }
+    //   ]);
+
+    // if (upsertError) {
+    //   throw upsertError;
+    // }
+
+    return imageUrl;
   } catch (error) {
     throw error;
   }
 };
+
 
 
